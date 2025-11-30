42a6fe8d24d7f80586eda11d36cc91a6e189dc89
jeferson Nascimento <jeffersom732@gmail.com>
2025-11-30 19:55:14 -0300

    bugs checkbox

--- src/script.js
+++ src/script.js
@@ -14,14 +14,13 @@ window.onload = load()
 
 input.focus();
 
-function createItem(item) {
+function createItem(item, id, check) {
   const liElement = document.createElement('li');
   liElement.classList.add('item');
-  idItem++
 
   liElement.innerHTML = `
     <label class="item_content">
-      <input type="checkbox" id="item-${idItem}" name="itens" value="${item}" />
+      <input type="checkbox" id="item-${id}" name="itens" value="${item}" ${check ? "checked" : ""}/>
       <span>${item}</span>
     </label>
 
@@ -33,7 +32,7 @@ function createItem(item) {
   listItems.prepend(liElement);
 }
 
-function loadLocalStorage(){
+function loadLocalStorage() {
   const data = localStorage.getItem("LIST")
   return data ? JSON.parse(data) : []
 }
@@ -42,15 +41,21 @@ function load() {
   listItems.innerHTML = ""
   const list = loadLocalStorage()
   list.forEach(item => {
-    createItem(item)
+    if(item.selected){
+      createItem(item.name, item.id, item.selected)
+    } else {
+      createItem(item.name, item.id, item.selected)
+    }
   })
 }
 
-function saveItem(item){
+function saveItem(item) {
   const list = loadLocalStorage()
   list.push(item)
 
   localStorage.setItem("LIST", JSON.stringify(list))
+
+  load()
 
 }
 
@@ -61,12 +66,17 @@ btnSubmit.addEventListener('click', (event) => {
     notification.classList.remove('hidden');
     message.innerHTML = `<p>Digite um item para adicionar</p>`;
     notification.classList.add('show');
-    
+
     return;
   }
 
-  saveItem(input.value)
-  load()
+  const item = {
+    id: idItem++,
+    name: input.value,
+    selected: false
+  }
+
+  saveItem(item)
 
   input.value = '';
   input.focus();
@@ -78,11 +88,11 @@ listItems.addEventListener('click', (event) => {
 
   const itemToRemove = event.target.closest('.item');
   const nameItem = itemToRemove.querySelector('span').textContent;
-  
+
   const list = loadLocalStorage()
 
   const newList = list.filter(item => {
-    return item != nameItem
+    return item.name != nameItem
   })
 
   localStorage.setItem("LIST", JSON.stringify(newList))
@@ -94,6 +104,21 @@ listItems.addEventListener('click', (event) => {
   message.innerHTML = `<p>O item <strong>${nameItem}</strong> foi removido da lista</p>`;
 
 
+});
+
+listItems.addEventListener('click', (event) => {
+  const isCheck = event.target.closest('input[type="checkbox"]');;
+  if (!isCheck) return;
+
+  const itemName = (event.target.nextElementSibling).innerText
+  const itens = loadLocalStorage()
+
+  const newList = itens.map(item => {
+    return item.name === itemName ? { ...item, selected: item.selected ? false : true } : item       
+  })
+
+  localStorage.setItem("LIST", JSON.stringify(newList))
+  
 });
 
 btnCloseNoti.addEventListener('click', () => {
